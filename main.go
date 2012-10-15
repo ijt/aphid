55595825df333553507b01bad8a1356061138b7e
Issac Trotts <itrotts@willowgarage.com>
2012-10-14 23:22:20 -0700

    Allow configurable message_prefix.
    Set default to cyan [catkin_sleuth]

--- main.go
+++ main.go
@@ -12,6 +12,7 @@ import (
 )
 
 type Config struct {
+	Message_prefix string
 	Line_rules []*LineRule
 }
 
@@ -53,6 +54,10 @@ func loadConfig(url string) *Config {
 		os.Exit(1)
 	}
 
+	if conf.Message_prefix == "" {
+		conf.Message_prefix = "  " + FgCyan + "[catkin_sleuth]" + Reset
+	}
+
 	// Compile the regexes
 	for _, rule := range conf.Line_rules {
 		rule.patternRx = regexp.MustCompile(rule.Pattern)
@@ -81,9 +86,37 @@ func addHelp(reader *bufio.Reader, conf *Config) {
 			if matched {
 				// FIXME: Substitute positional references from
 				// regexp
-				fmt.Println(rule.Message)	
+				fmt.Print(conf.Message_prefix, " ", rule.Message)	
 			}
 		}
 	}
 }
+
+const (
+        Reset = "\x1b[0m"
+        Bright = "\x1b[1m"
+        Dim = "\x1b[2m"
+        Underscore = "\x1b[4m"
+        Blink = "\x1b[5m"
+        Reverse = "\x1b[7m"
+        Hidden = "\x1b[8m"
+
+        FgBlack = "\x1b[30m"
+        FgRed = "\x1b[31m"
+        FgGreen = "\x1b[32m"
+        FgYellow = "\x1b[33m"
+        FgBlue = "\x1b[34m"
+        FgMagenta = "\x1b[35m"
+        FgCyan = "\x1b[36m"
+        FgWhite = "\x1b[37m"
+
+        BgBlack = "\x1b[40m"
+        BgRed = "\x1b[41m"
+        BgGreen = "\x1b[42m"
+        BgYellow = "\x1b[43m"
+        BgBlue = "\x1b[44m"
+        BgMagenta = "\x1b[45m"
+        BgCyan = "\x1b[46m"
+        BgWhite = "\x1b[47m"
+)
 
